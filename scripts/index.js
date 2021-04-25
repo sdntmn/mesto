19c2d6da5f06fa878c9abac70c5f50bf31210b6f
sdntmn <67434735+sdntmn@users.noreply.github.com>
2021-04-26 03:20:57 +0500

    fix: review-1
    Правки после первого ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,25 @@
-const openPopupButton = document.querySelector(".profile__opened");
+let formElement = document.querySelector(".profile");
 
+const openPopupButton = formElement.querySelector(".profile__opened");
 const popup = document.querySelector(".popup");
-const closePopupButton = document.querySelector(".popup__close");
+const closePopupButton = popup.querySelector(".popup__close");
 
-function togglePopup(event) {
-  event.preventDefault();
+let nameInput = popup.querySelector(".popup__input_value_name");
+let jobInput = popup.querySelector(".popup__input_value_job");
+let newJob = formElement.querySelector(".profile__specialization");
+let newName = formElement.querySelector(".profile__item-info");
+
+function openPopup() {
+  popup.classList.toggle("popup_opened");
+  nameInput.value = newName.textContent;
+  jobInput.value = newJob.textContent;
+}
+openPopupButton.addEventListener("click", openPopup);
+
+function closePopup() {
   popup.classList.toggle("popup_opened");
 }
-openPopupButton.addEventListener("click", togglePopup);
-closePopupButton.addEventListener("click", togglePopup);
+closePopupButton.addEventListener("click", closePopup);
 
 function handleOverlayClick(event) {
   if (event.target === event.currentTarget) {
@@ -17,17 +28,10 @@ function handleOverlayClick(event) {
 }
 popup.addEventListener("click", handleOverlayClick);
 
-let formElement = document.querySelector(".profile");
-let nameInput = popup.querySelector(".popup__input_value_name");
-let jobInput = popup.querySelector(".popup__input_value_job");
-let newJob = formElement.querySelector(".profile__specialization");
-let newName = formElement.querySelector(".profile__item-info");
-
 function formSubmitHandler(evt) {
   evt.preventDefault();
-
-  newJob.textContent = jobInput.value;
   newName.textContent = nameInput.value;
-  togglePopup(evt);
+  newJob.textContent = jobInput.value;
+  closePopup(evt);
 }
 popup.addEventListener("submit", formSubmitHandler);
