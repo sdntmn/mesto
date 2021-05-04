df22a578fb6c7801c139a29ef92b7c675fe9a30f
sdntmn <67434735+sdntmn@users.noreply.github.com>
2021-05-04 17:00:29 +0500

    feat: card
    Добавлена функция первоначального вывода карточек, добавление новых карточек.

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,10 @@
 const initialCards = [
   {
+    name: "Байкал",
+    link:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+  {
     name: "Архыз",
     link:
       "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
@@ -24,28 +29,59 @@ const initialCards = [
     link:
       "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
   },
-  {
-    name: "Байкал",
-    link:
-      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
-  },
 ];
 
 let formElement = document.querySelector(".profile");
 
 const openPopupBtnProfile = formElement.querySelector(".profile__opened");
 const openPopupBtnMesto = formElement.querySelector(".profile__button");
+
+const elementTemplate = document.querySelector("#template-element").content;
+
+const openPopupClickMesto = elementTemplate.querySelector(".element__img");
 const popupProfile = document.querySelector("#popup_form_profile");
 const popupMesto = document.querySelector("#popup_form_mesto");
+const popupFotoMesto = document.querySelector("#popup_foto_mesto");
+
 const closePopupBtnProfile = popupProfile.querySelector(".popup__close");
 const closePopupBtnMesto = popupMesto.querySelector(".popup__close");
+const saveBtnMesto = popupMesto.querySelector(".popup__button");
+
+const listMesto = document.querySelector(".elements");
+
+const inputMesto = popupMesto.querySelector(".popup__input_value_mesto");
+const inputLink = popupMesto.querySelector(".popup__input_value_link");
 
 let nameInput = popupProfile.querySelector(".popup__input_value_name");
 let jobInput = popupProfile.querySelector(".popup__input_value_job");
 let newJob = formElement.querySelector(".profile__specialization");
 let newName = formElement.querySelector(".profile__item-info");
 
-/* Ввод данных карточек и работа с темплейт  */
+/* Первоначальный вывод карточек из массива */
+initialCards.forEach(function (element) {
+  const cardElement = elementTemplate.cloneNode(true);
+  const imgElemTemplate = cardElement.querySelector(".element__img");
+  const textElemTemplate = cardElement.querySelector(".element__name-mesto");
+
+  imgElemTemplate.src = element.link;
+  imgElemTemplate.alt = `Фото. ` + element.name;
+  textElemTemplate.textContent = element.name;
+  listMesto.append(cardElement);
+});
+
+/* Добавление новых карточек */
+saveBtnMesto.addEventListener("click", function (e) {
+  e.preventDefault();
+  const cardElement = elementTemplate.cloneNode(true);
+  const imgElemTemplate = cardElement.querySelector(".element__img");
+  const textElemTemplate = cardElement.querySelector(".element__name-mesto");
+
+  textElemTemplate.textContent = inputMesto.value;
+  imgElemTemplate.src = inputLink.value;
+  imgElemTemplate.alt = `Фото. ` + inputMesto.value;
+  listMesto.append(cardElement);
+  closePopupMesto(e);
+});
 
 /* Открытие popupProfile */
 function openPopupProfile() {
@@ -98,19 +134,10 @@ function formSubmitHandlerProfile(evt) {
 }
 popupProfile.addEventListener("submit", formSubmitHandlerProfile);
 
-/* Добавление карточек и закрытие popupMesto по кнопке создать*/
-const elementTemplate = document.querySelector("#template-element").content;
-const inputMesto = popupMesto.querySelector(".popup__input_value_mesto");
-const inputLink = popupMesto.querySelector(".popup__input_value_link");
-const listMesto = document.querySelector(".elements");
-
-initialCards.forEach(function (element) {
-  const cardElement = elementTemplate.cloneNode(true);
-  const img = cardElement.querySelector(".element__img");
-  const name = cardElement.querySelector(".element__name-mesto");
-  img.src = element.link;
-  console.log(img.src);
-  name.textContent = element.name;
-  console.log(name.textContent);
-  listMesto.append(cardElement);
-});
+/* Открытие popupFoto 
+const imgElem = elementTemplate.querySelector(".element__img");
+function openPopupBigFoto() {
+  popupFotoMesto.classList.toggle("popup_is-opened");
+}
+imgElem.addEventListener("click", openPopupBigFoto);
+*/
