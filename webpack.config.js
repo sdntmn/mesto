e6c35583316712a53f48cd26b0f1802f7b82a99d
sdntmn <67434735+sdntmn@users.noreply.github.com>
2021-07-17 14:45:55 +0500

    reName
    Переименование файлов

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: "./pages/index.js",
@@ -39,6 +40,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({ template: "index.html" }),
+    new CleanWebpackPlugin(),
     new MiniCssExtractPlugin(),
   ],
 };
